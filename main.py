1438258d59314ad69dcb0ad5f8437d0974150f77
Adam <57396704+Sankti@users.noreply.github.com>
2020-01-16 16:00:47 +0100

    Version 1.0
    All functionalities stable.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from threading import Thread
 running = False
  
 window = Tk()
-window.title("Majcurber 3000")
+window.title("Majec Disturber")
 window.geometry('300x360')
 
 majec1_img = PhotoImage(file="img\Majec_1.gif")
@@ -16,10 +16,10 @@ majec3_photo_label = Label(window, image=majec3_img)
 majec4_img = PhotoImage(file="img\Majec_4.gif")
 majec4_photo_label = Label(window, image=majec4_img)
  
-title = Label(window, text="Majcurber 3000®", fg="brown", font=("Times New Roman", 28, "bold"))
-undertitle = Label(window, text="The Universal Majec Disturber™", fg="black", font=("arial", 14))
+title = Label(window, text="Majec Disturber®", fg="brown", font=("Times New Roman", 28, "bold"))
+undertitle = Label(window, text="Raging Majec Simulator™", fg="black", font=("arial", 14))
 copyright = Label(window, text="©2020 King Adam Corporation", fg="grey", font=("arial", 8))
-message_box = Text(window, width=25, height=3, wrap=WORD, fg="black", font=("Courier New", 10))
+message_box = Text(window, width=25, height=2, wrap=WORD, fg="black", font=("Courier New", 10))
 button = Button(window, text="Disturb Majec", bg="khaki", fg="dim gray", font=("Courier New", 14, "bold"))
 message_box.config(state=DISABLED)
 
@@ -33,13 +33,33 @@ rage_level = 0
 
 def start_count(event):
     global running
-    write("\n" + "Commencing disturbance...")
+    write("\n" + "\n" + "Commencing disturbance...")
     running = True
+    majec2_photo_label.grid(row=3, columnspan=3, pady=20)
+    global rage_level
+    rage_level += 1
+	
+def evaluate():
+	majec1_photo_label.grid_forget()
+	majec2_photo_label.grid_forget()
+	majec3_photo_label.grid_forget()
+	majec4_photo_label.grid_forget()
+	
+	if rage_level < 3:
+		majec1_photo_label.grid(row=3, columnspan=3, pady=20)
+		write("\n" + "\n" + "Majec is unamused")
+	elif rage_level < 5:
+		majec3_photo_label.grid(row=3, columnspan=3, pady=20)
+		write("\n" + "\n" + "Majec is raging!")
+	else:
+		majec4_photo_label.grid(row=3, columnspan=3, pady=20)
+		write("\n" + "\n" + "Majec has had it!!!")
 
 def stop_count(event):
     global running
     running = False
-		
+    evaluate()
+
 button.bind('<ButtonPress-1>',start_count)
 button.bind('<ButtonRelease-1>',stop_count)
 
