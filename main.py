4486d015c528d379def3141f6eb32be1c7abe261
Adam <57396704+Sankti@users.noreply.github.com>
2020-01-16 15:41:24 +0100

    Version 0.2
    Added button hold and release functionality.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
-import time
+from threading import Thread
+ 
+running = False
  
 window = Tk()
 window.title("Majcurber 3000")
@@ -17,8 +19,8 @@ majec4_photo_label = Label(window, image=majec4_img)
 title = Label(window, text="Majcurber 3000®", fg="brown", font=("Times New Roman", 28, "bold"))
 undertitle = Label(window, text="The Universal Majec Disturber™", fg="black", font=("arial", 14))
 copyright = Label(window, text="©2020 King Adam Corporation", fg="grey", font=("arial", 8))
-message_box = Text(window, width=25, height=2, wrap=WORD, fg="black", font=("Courier New", 10))
-button = Button(window, text="Disturb Majec", bg="khaki", fg="dim gray", font=("Courier New", 14, "bold"), command=lambda:randomize())
+message_box = Text(window, width=25, height=3, wrap=WORD, fg="black", font=("Courier New", 10))
+button = Button(window, text="Disturb Majec", bg="khaki", fg="dim gray", font=("Courier New", 14, "bold"))
 message_box.config(state=DISABLED)
 
 def write(string):
@@ -26,11 +28,20 @@ def write(string):
     message_box.insert("end", string)
     message_box.see("end")
     message_box.config(state=DISABLED)
-	
-start_button = time.time()
 
-def disturb():
-	majec1_label.grid_forget()
+rage_level = 0
+
+def start_count(event):
+    global running
+    write("\n" + "Commencing disturbance...")
+    running = True
+
+def stop_count(event):
+    global running
+    running = False
+		
+button.bind('<ButtonPress-1>',start_count)
+button.bind('<ButtonRelease-1>',stop_count)
 
 title.grid(row=0, columnspan=3)
 undertitle.grid(row=1, columnspan=3)
@@ -39,6 +50,5 @@ majec1_photo_label.grid(row=3, columnspan=3, pady=20)
 message_box.grid(row=4, columnspan=3, pady=20)
 button.grid(row=5, column=1)
 write("Hold the button to enhance disturbance.")
-write(start_button)
  
 window.mainloop()
